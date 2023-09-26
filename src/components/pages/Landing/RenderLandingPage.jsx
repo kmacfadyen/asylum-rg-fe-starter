dc99f6dd6d675571af5ebc69ceb71044bcc64411
Kevin MacFadyen <kmacfadyen3@gmail.com>
2023-09-26 18:51:40 -0400

    checkpoint

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
@@ -32,7 +32,17 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
+      <div className="graphs-section">
+        <img src={GrantRatesByOfficeImg} />
+        <img src={GrantRatesByNationalityImg} />
+        <img src={GrantRatesOverTimeImg} />
+      </div>
+      <div className="graphs-section-text">
+        <p>Search Grant Rates By Office</p>
+        <p>Search Grant Rates By Nationality</p>
+        <p>Search Grant Rates Over Time</p>
+      </div>
+
       <div className="view-more-data-btn-container">
         <Button
           type="default"
@@ -40,6 +50,13 @@ function RenderLandingPage(props) {
           onClick={() => history.push('/graphs')}
         >
           View the Data
+        </Button>
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+          onClick={() => history.push('/graphs')}
+        >
+          Download the Data
         </Button>
       </div>
 
