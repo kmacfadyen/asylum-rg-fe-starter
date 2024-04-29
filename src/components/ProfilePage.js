8e4eb008763434fba786ea740e13ab4296bc15a8
Kevin MacFadyen <kmacfadyen3@gmail.com>
2024-04-28 23:58:40 -0400

    added profile page

---
+++ src/components/ProfilePage.js
@@ -0,0 +1,14 @@
+// src/components/ProfilePage.js
+
+import React from 'react';
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      {/* Add profile details here */}
+    </div>
+  );
+};
+
+export default ProfilePage;
